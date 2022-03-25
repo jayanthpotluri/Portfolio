050b3297c66167e5e0fa30b8f7a116a5f4d51bfe
jayanthpotluri <jayanthpotluri@gmail.com>
2022-03-25 11:04:38 +0530

    stage1

---
+++ script.js
@@ -0,0 +1,12 @@
+var j=0;
+function wallet(){
+
+    if(j>=2)
+    {
+        var a = document.getElementById('picture');
+        a.src = "jayanth.jpg";
+    }
+    j++;
+    console.log(j);
+    
+}
